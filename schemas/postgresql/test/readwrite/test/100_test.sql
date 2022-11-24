7352abd998da89f3581815277606d2adb8b2b9fe
Eric Hosick <erichosick@gmail.com>
2022-11-23 16:01:05 -0800

    Add a test to the test schema.

--- schemas/postgresql/test/readwrite/test/100_test.sql
+++ schemas/postgresql/test/readwrite/test/100_test.sql
@@ -1,6 +1,14 @@
+DO $$
+BEGIN
 
 -- -----------------------------------------------------------------------------
 -- test - test
 -- 
 -- -----------------------------------------------------------------------------
 
+  PERFORM test.it_should_exception('
+    RAISE EXCEPTION "A url should be case-insensitive.";',
+    'It should catch an exception'
+  );
+
+END $$;
